c4e087fb2b46b18dcc214e97971077fb59b89634
StavLobel <stavlobel@gmail.com>
2019-04-10 20:59:13 +0300

    Finished main screen

--- graphics/CityFrame.java
+++ graphics/CityFrame.java
@@ -12,6 +12,13 @@ public class CityFrame extends JFrame{
 	public static final String TITLE = "City";
 	public static final String FILE_TITLE = "File";
 	public static final String HELP_TITLE = "Help";
+	public static final String ADD_VEHICLE_LABEL = "Add Vehicle";
+	public static final String CLEAR_LABEL = "Clear";
+	public static final String FUEL_OR_FOOD_LABEL = "Fuel/Food";
+	public static final String LIGHTS_LABEL = "Lights";
+	public static final String INFO_LABEL = "Info";
+	public static final String EXIT_LABEL = "Exit";
+	public static final String[] BOTTTOM_PANEL_LABELS = {ADD_VEHICLE_LABEL,CLEAR_LABEL,FUEL_OR_FOOD_LABEL,LIGHTS_LABEL,INFO_LABEL,EXIT_LABEL};
 	public static BufferedImage backGround = null;
 	
 	public boolean setBackground() {
@@ -39,7 +46,7 @@ public class CityFrame extends JFrame{
 	
 	public static JMenuBar createMenu() {
 		JMenuBar menuBar;
-		JMenu menu, secmenu;
+		JMenu menu;
 		JMenuItem menuItem;
 		
 		menuBar = new JMenuBar();
@@ -47,7 +54,7 @@ public class CityFrame extends JFrame{
 		menu = new JMenu(FILE_TITLE);
 		menuBar.add(menu);
 		
-		menuItem = new JMenuItem(new AbstractAction("Exit"){
+		menuItem = new JMenuItem(new AbstractAction(EXIT_LABEL){
 		    public void actionPerformed(ActionEvent e) {
 		        System.exit(0);
 		    }
@@ -67,10 +74,22 @@ public class CityFrame extends JFrame{
 		return menuBar;
 	}
 	
+	public Boolean createBottomPanel(){
+		JPanel bottomPanel = new JPanel();
+		JButton[] buttons = new JButton[BOTTTOM_PANEL_LABELS.length];
+		for (int i=0 ; i < buttons.length ; ++i ) {
+			buttons[i] = new JButton(BOTTTOM_PANEL_LABELS[i]);
+			bottomPanel.add(buttons[i]);
+		}
+		this.add(bottomPanel,BorderLayout.SOUTH);
+		return true;
+	}
+	
 	public static void main(String[] args) {
 		frame = new CityFrame(TITLE);
 		frame.setJMenuBar(createMenu());
 		frame.setBackground();
+		frame.createBottomPanel();
 		frame.pack();  
 	}
 }
